b33d07730c53f2a2a6d26dfd4b41e442f8ae6309
pelumi omotesho <pelumi066@gmail.com>
2018-03-26 23:07:05 +0100

    Add delete script

---
+++ playgrounds/mongodb-delete.js
@@ -0,0 +1,43 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Cannot connect to mongo servers', err);
+    }
+    console.log('Accessing db');
+
+    //delete many 
+
+    // db.collection('Todo').deleteMany({ text: "Read x-men" }).then((result) => {
+    //     console.log(result);
+    // });
+
+    //deleteOne
+
+    // db.collection('Todo').deleteOne({ text: "Read batman" }).then((result) => {
+    //     console.log(result);
+    // });
+
+     //findOneAndDelete
+    // db.collection('Todo').findOneAndDelete({completed:false}).then((result) => {
+    //     console.log(result);
+        
+    // })
+
+    //////////////
+    //CHALLENGE//
+    ////////////
+
+    // db.collection('user').deleteMany({name:'pelumi'}).then((result) => {
+    //     console.log(result);
+    // })
+
+    db.collection('user').findOneAndDelete({ _id: new ObjectId("5ab868b7b8ba78181864f667")}).then((result) => {
+        console.log(JSON.stringify(result,undefined,2));
+        
+    });
+
+
+
+    // db.close()
+});
